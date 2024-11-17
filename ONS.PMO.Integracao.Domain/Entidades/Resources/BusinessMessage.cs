69e6cfa601fe29764cf9b391323b9eb6dba547e7
Fabio Sander <fabiosandernt@gmail.com>
2024-11-17 17:13:16 -0300

    add mensagens de negocio

---
+++ ONS.PMO.Integracao.Domain/Entidades/Resources/BusinessMessage.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+
+namespace ONS.PMO.Integracao.Domain.Entidades.Resources
+{
+    public class BusinessMessage
+    {
+        public string Code { get; set; }
+        public string Value { get; set; }
+        public string Comment { get; set; }
+
+        private static List<BusinessMessage> Messages;
+
+        // Caminho para o arquivo JSON
+        private static readonly string JsonFilePath = "businessMessage.json";
+
+        static BusinessMessage()
+        {
+            LoadMessagesFromFile();
+        }
+
+        // Método para carregar mensagens do arquivo JSON
+        private static void LoadMessagesFromFile()
+        {
+            if (File.Exists(JsonFilePath))
+            {
+                var json = File.ReadAllText(JsonFilePath);
+                Messages = JsonConvert.DeserializeObject<List<BusinessMessage>>(json);
+            }
+            else
+            {
+                Messages = new List<BusinessMessage>();
+            }
+        }
+
+        // Busca uma mensagem pelo código
+        public static BusinessMessage Get(string code)
+        {
+            return Messages.FirstOrDefault(m => m.Code == code);
+        }
+    }
+
+}
